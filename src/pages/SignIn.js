8b9a03cd419699b13f1de81609d18abab76711fd
Pratik Patekar <pratik.patekar3@gmail.com>
2020-12-26 13:06:32 +0530

    managed user with context api

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Panel, Row, Col, Button, Icon, Alert } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const SignIn = () => {
-  return <div>Sign in</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+      Alert.success('Signed In', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>SignIn</h2>
+                <p>Chat platform for someone, hopefully!</p>
+              </div>
+              <div className="mt-3">
+                <Button block color="red" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
